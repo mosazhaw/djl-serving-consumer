151ede770b80febe8f9f4eccd36254c4f280b04c
Adrian Moser <mosa@zhaw.ch>
2022-12-06 17:01:48 +0100

    add

--- src/main/java/ch/zhaw/djl/consumer/consumer/ConsumerController.java
+++ src/main/java/ch/zhaw/djl/consumer/consumer/ConsumerController.java
@@ -1,8 +1,26 @@
 package ch.zhaw.djl.consumer.consumer;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.InputStream;
+import java.nio.channels.Channels;
 
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferUtils;
+import org.springframework.core.io.buffer.DefaultDataBufferFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.reactive.function.BodyInserter;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.core.publisher.Flux;
 
 @RestController
 public class ConsumerController {
@@ -10,6 +28,25 @@ public class ConsumerController {
     @GetMapping("/ping")
     public String ping() {
         return "DJL Consumer app is up and running!";
+    }
+
+    @PostMapping(path = "/analyze")
+    public String predict(@RequestParam("image") MultipartFile image) throws Exception {
+        InputStream is = new ByteArrayInputStream(image.getBytes());
+
+        var webClient = WebClient.create();
+        Resource resource = new InputStreamResource(is);
+        var result = webClient.post()
+                .uri("http://localhost:8080/predictions/resnet18_v1")
+                .contentType(MediaType.MULTIPART_FORM_DATA)
+                .body(BodyInserters.fromResource(resource))
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+        System.out.println(result);
+        System.out.println(result);
+
+        return "dummy";
     }
 
 }
