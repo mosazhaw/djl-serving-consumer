215498ffccc44dd02df3b5269e6cd893d16813c3
Adrian Moser <mosa@zhaw.ch>
2022-12-07 09:30:18 +0100

    update

--- src/main/java/ch/zhaw/djl/consumer/consumer/ConsumerController.java
+++ src/main/java/ch/zhaw/djl/consumer/consumer/ConsumerController.java
@@ -3,24 +3,17 @@ package ch.zhaw.djl.consumer.consumer;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.InputStream;
-import java.nio.channels.Channels;
 
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.buffer.DataBuffer;
-import org.springframework.core.io.buffer.DataBufferUtils;
-import org.springframework.core.io.buffer.DefaultDataBufferFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.reactive.function.BodyInserter;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import reactor.core.publisher.Flux;
 
 @RestController
 public class ConsumerController {
@@ -34,16 +27,26 @@ public class ConsumerController {
     public String predict(@RequestParam("image") MultipartFile image) throws Exception {
         InputStream is = new ByteArrayInputStream(image.getBytes());
 
+        var uri = "http://localhost:8080/predictions/resnet18_v1";
+        if (this.isDockerized()) {
+            uri = "http://model-service:8080/predictions/resnet18_v1";
+        }
+
         var webClient = WebClient.create();
         Resource resource = new InputStreamResource(is);
         var result = webClient.post()
-                .uri("http://localhost:8080/predictions/resnet18_v1")
+                .uri(uri)
                 .contentType(MediaType.MULTIPART_FORM_DATA)
                 .body(BodyInserters.fromResource(resource))
                 .retrieve()
                 .bodyToMono(String.class)
                 .block();
         return result;
+    }
+
+    private boolean isDockerized() {
+        File f = new File("/.dockerenv");
+        return f.exists();
     }
 
 }
